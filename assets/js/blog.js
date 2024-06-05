ca726135d1aaa055d6af13f3656563eeb442a556
AssistedWind <adam.mathis05@gmail.com>
2024-06-05 17:38:08 -0500

    finished back button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,7 +1,36 @@
 // DEPENDENCIES
 
+const returnHome = document.getElementById('goBack');
+
+
 
 // DATA
+
+
+
+
+
+// FUNCTIONS
+
+
+
+
+function homeRedirect () {
+    window.location.href="index.html";
+
+}
+
+// USER INTERACTIONS
+
+returnHome.addEventListener('click', () => {
+    homeRedirect()
+
+});
+
+
+
+// INITIALIZATION
+
 
 
 
