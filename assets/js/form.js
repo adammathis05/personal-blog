4171f0c0386f80a03b8b2bd2f02f9928079480e3
AssistedWind <adam.mathis05@gmail.com>
2024-06-05 16:42:10 -0500

    dark mode done

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,9 @@
 // DEPENDENCIES
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('.container');
+// const themeSwitcher = document.querySelector('#theme-switcher');
+// const container = document.querySelector('.container');
 
-const modeToggle = document.getElementById('toggle');
-const body = document.body;
+// const modeToggle = document.getElementById('toggle');
+// const body = document.body;
 
 // DATA
 
@@ -44,10 +44,15 @@ function handleRedirect () {
 }
 
 
-modeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-});
+// modeToggle.addEventListener('click', () => {
+
+//     // are we in dark mode?
+//         // yes? remove dark mode styles & add light mode styles
+//         // not? remove light mode styles & add dark mode styles
+
+//     body.classList.toggle('dark-mode');
+//     body.classList.toggle('light-mode');
+// });
 
 // USER INTERACTIONS 
 
