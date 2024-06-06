e573a1c0570f4a1fe913591478a8b59a13a9577f
AssistedWind <adam.mathis05@gmail.com>
2024-06-05 21:41:37 -0500

    pre-submission

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,7 +1,10 @@
 // DEPENDENCIES
 
 const returnHome = document.getElementById('goBack');
+//break
 
+
+//break
 
 
 // DATA
@@ -11,18 +14,31 @@ let blogs = storedBlogs ? JSON.parse(storedBlogs) : [];
 
 // let newBlog = JSON.parse(localStorage.getItem('blogs'));
 
+function renderLastRegistered() {
+    const renderNewBlog = localStorage.getItem(storedBlogs);
+    
+}
 
-localStorage.getItem(storedBlogs);
+
 
 
 console.log(storedBlogs);
 
 
 // FUNCTIONS
+// break
+
+
+renderLastRegistered();
+
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
+}
 
 
 
-
+// break
 function homeRedirect () {
     window.location.href="index.html";
 
