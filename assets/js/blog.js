534d69b4be35748ebe9f6cb5549996c16e2f6fe2
AssistedWind <adam.mathis05@gmail.com>
2024-06-05 21:06:32 -0500

    mid-blog commit

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,8 +6,16 @@ const returnHome = document.getElementById('goBack');
 
 // DATA
 
+let storedBlogs = localStorage.getItem("blogs");
+let blogs = storedBlogs ? JSON.parse(storedBlogs) : [];
+
+// let newBlog = JSON.parse(localStorage.getItem('blogs'));
 
 
+localStorage.getItem(storedBlogs);
+
+
+console.log(storedBlogs);
 
 
 // FUNCTIONS
